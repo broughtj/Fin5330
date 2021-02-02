55525484cfc54d0df25aa4a497c727b6a9503385
broughtj <broughtj@gmail.com>
2021-02-01 23:31:54 -0700

    Adding Chp2 Jupyter notebook.

---
+++ Spring2021/Projects/Project1/Julia/.ipynb_checkpoints/Chapter02-checkpoint.jl
@@ -0,0 +1,147 @@
+### A Pluto.jl notebook ###
+# v0.12.20
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 1e6e6c52-6517-11eb-14fa-9d7acca29def
+md"""
+# **Chapter 02: Statistical Properties of Financial Data**
+"""
+
+# ╔═╡ 3e58508e-6517-11eb-0736-330fb093d5ce
+md"""
+## Section 2.1: A First Look At The Data
+"""
+
+# ╔═╡ 525fe8d8-6517-11eb-2471-6da52cd90a90
+md"""
+### 2.1.1 Prices
+"""
+
+# ╔═╡ 5a74449c-6517-11eb-2fce-9f4a2bebdfb4
+md"""
+### 2.1.2 Returns
+"""
+
+# ╔═╡ 6c912802-6517-11eb-0b17-4757da3d25d7
+md"""
+### 2.1.3 Dividends
+"""
+
+# ╔═╡ 76f4774a-6517-11eb-0cf8-11457586cb83
+md"""
+### 2.1.4 Bond Yields
+"""
+
+# ╔═╡ 8467eba0-6517-11eb-35cc-1d64d75a1a4d
+md"""
+### 2.1.5 Financial Distributions
+"""
+
+# ╔═╡ 93b9288a-6517-11eb-2bea-bbcdd4a5a161
+md"""
+### 2.1.6 Transactions
+"""
+
+# ╔═╡ a82a1a72-6517-11eb-158e-3d7ebca7a968
+md"""
+## Section 2.2: Summary Statistics
+"""
+
+# ╔═╡ b5bec76e-6517-11eb-144c-9565ab4a6ef3
+md"""
+### 2.2.1 Univariate
+"""
+
+# ╔═╡ c4169daa-6517-11eb-118a-f14c96d92f80
+md"""
+##### **Sample Mean**
+"""
+
+# ╔═╡ dc88834e-6517-11eb-1598-11670ac70421
+md"""
+##### **Sample Variance and Standard Deviation**
+"""
+
+# ╔═╡ ed019f4c-6517-11eb-08f8-51d5e2d44455
+md"""
+##### **Sample Skewness**
+"""
+
+# ╔═╡ 00a5589c-6518-11eb-2f90-4d835abd4143
+md"""
+##### **Sample Kurtosis**
+"""
+
+# ╔═╡ 13027d80-6518-11eb-12f1-81bc542d5d1d
+md"""
+### 2.2.2 Bivariate
+"""
+
+# ╔═╡ 2460981e-6518-11eb-37e5-c79d76563c9b
+md"""
+##### **Covariance**
+"""
+
+# ╔═╡ 2ee9c60c-6518-11eb-0a9b-436872432d19
+md"""
+##### **Correlation**
+"""
+
+# ╔═╡ 374b65b2-6518-11eb-37c8-ffb48589029a
+md"""
+## Section 2.3: Percentiles and Value at Risk
+"""
+
+# ╔═╡ 61fb33d4-6518-11eb-0edd-33390ce42180
+md"""
+1. Historical Simulation
+2. The Variance Method
+3. Monte Carlo Simulation
+"""
+
+# ╔═╡ 77dccfb2-6518-11eb-3b6d-59064b8dc68d
+md"""
+## Section 2.4: The Efficient Market Hypothesis
+"""
+
+# ╔═╡ 83b722e2-6518-11eb-38ac-8fd421ffb595
+md"""
+### 2.4.1 Return Predictability
+"""
+
+# ╔═╡ 913339ce-6518-11eb-2807-99ecf168a443
+md"""
+### 2.4.2 The Variance Ratio
+"""
+
+# ╔═╡ a194e95a-6518-11eb-378b-a9f6e1eda4a6
+md"""
+## Section 2.5: Exercises
+"""
+
+# ╔═╡ Cell order:
+# ╟─1e6e6c52-6517-11eb-14fa-9d7acca29def
+# ╟─3e58508e-6517-11eb-0736-330fb093d5ce
+# ╟─525fe8d8-6517-11eb-2471-6da52cd90a90
+# ╟─5a74449c-6517-11eb-2fce-9f4a2bebdfb4
+# ╟─6c912802-6517-11eb-0b17-4757da3d25d7
+# ╟─76f4774a-6517-11eb-0cf8-11457586cb83
+# ╟─8467eba0-6517-11eb-35cc-1d64d75a1a4d
+# ╟─93b9288a-6517-11eb-2bea-bbcdd4a5a161
+# ╟─a82a1a72-6517-11eb-158e-3d7ebca7a968
+# ╟─b5bec76e-6517-11eb-144c-9565ab4a6ef3
+# ╟─c4169daa-6517-11eb-118a-f14c96d92f80
+# ╟─dc88834e-6517-11eb-1598-11670ac70421
+# ╟─ed019f4c-6517-11eb-08f8-51d5e2d44455
+# ╟─00a5589c-6518-11eb-2f90-4d835abd4143
+# ╟─13027d80-6518-11eb-12f1-81bc542d5d1d
+# ╟─2460981e-6518-11eb-37e5-c79d76563c9b
+# ╟─2ee9c60c-6518-11eb-0a9b-436872432d19
+# ╟─374b65b2-6518-11eb-37c8-ffb48589029a
+# ╟─61fb33d4-6518-11eb-0edd-33390ce42180
+# ╟─77dccfb2-6518-11eb-3b6d-59064b8dc68d
+# ╟─83b722e2-6518-11eb-38ac-8fd421ffb595
+# ╟─913339ce-6518-11eb-2807-99ecf168a443
+# ╟─a194e95a-6518-11eb-378b-a9f6e1eda4a6
